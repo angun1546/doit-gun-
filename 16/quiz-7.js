c31f2ebfda7f48acfe908803dc94c71b85346e20
angun1546 <angun1546@gmail.com>
2026-01-21 16:51:41 +0900

    madeit!

---
+++ 16/quiz-7.js
@@ -0,0 +1,7 @@
+let output = "";
+for (let i = 1; i < 10; i++) {
+  for (let j = 0; j < i; j++) {
+    output = "\u002a";
+  }
+}
+console.log(output);
---
+++ 17/익명함수.js
@@ -0,0 +1,7 @@
+const sum = function (a, b) {
+  return a + b;
+};
+console.log(sum(1, 2));
+function multiple(a, b, c = 0) {
+  return a * b + c;
+}
